33a344d9c15ca0bfa46df6c07619141285edda0d
Juan David Pareja Soto <parejajd@outlook.com>
2018-08-21 22:14:34 -0500

    clase 2

---
+++ Codigo/Demo_01_Vector/Demo_01_Vector/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Demo_01_Vector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese la cantidad de estudiantes:");
+            int cantidad = Convert.ToInt32(Console.ReadLine());
+            //Inicialización
+            double[] notasFinales = new double[cantidad];
+            //llenado del arreglo
+            for (int i = 0; i < notasFinales.Length; i++)
+            {
+                Console.Write($"Ingrese la nota para estudiante {i + 1}:");
+                string dato = Console.ReadLine();
+                double valor = Convert.ToDouble(dato);
+                notasFinales[i] = valor;
+            }
+            double sumatoria = 0;
+            double promedio = 0;
+            //Lectura del arreglo
+            for (int i = 0; i < notasFinales.Length; i++)
+            {
+                sumatoria += notasFinales[i];
+            }
+            promedio = sumatoria / notasFinales.Length;
+
+            Console.WriteLine($"El promedio del curso es {promedio}");Estruc
+
+            Console.Read();
+        }
+    }
+}
---
+++ Codigo/Demo_01_Vector/Demo_02_Vector_Aleatorio/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+
+namespace Demo_02_Vector_Aleatorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese la cantidad de datos:");
+            int cantidad = Convert.ToInt32(Console.ReadLine());
+
+            int[] aleatorios = new int[cantidad];
+
+            Random random = new Random();
+            for (int i = 0; i < aleatorios.Length; i++)
+            {
+                aleatorios[i] = random.Next(1, 100);
+            }
+            Imprimir(aleatorios);
+            
+            Burbuja(aleatorios);
+
+            Imprimir(aleatorios);
+
+            Console.Read();
+        }
+
+        private static void Burbuja(int[] aleatorios)
+        {
+            int temporal;
+            for (int i = 0; i < aleatorios.Length; i++)
+            {
+                for (int j = 0; j < aleatorios.Length - 1; j++)
+                {
+                    if (aleatorios[j] > aleatorios[j + 1])
+                    {
+                        temporal = aleatorios[j];
+                        aleatorios[j] = aleatorios[j + 1];
+                        aleatorios[j + 1] = temporal;
+                    }
+                    Imprimir(aleatorios);
+                }
+            }
+        }
+
+        private static void Imprimir(int[] aleatorios)
+        {
+            Console.WriteLine();
+            for (int i = 0; i < aleatorios.Length; i++)
+            {
+                Console.Write(aleatorios[i] + "-");
+            }
+            Thread.Sleep(1000);
+        }
+    }
+}
